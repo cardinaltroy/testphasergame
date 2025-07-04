683368d46f68370ac7d706a3360f090b4c896c15
T R O Y <vovichek3@gmail.com>
2025-07-04 10:22:32 +0300

    Update UtilsGridScale.js

--- src/components/sceneGame/methods/UtilsGridScale.js
+++ src/components/sceneGame/methods/UtilsGridScale.js
@@ -1,4 +1,14 @@
 export function UtilsGridScale() {
-    const isMobile = this.sys.game.device.os.android || this.sys.game.device.os.iOS;
-    return isMobile ? 0.75 : 1.2;
+    const baseWidth = 1280;
+    const baseHeight = 720;
+
+    const screenWidth = this.sys.game.config.width;
+    const screenHeight = this.sys.game.config.height;
+
+    const scaleX = screenWidth / baseWidth;
+    const scaleY = screenHeight / baseHeight;
+
+    // Берём минимальное значение, чтобы всё влезало на экран
+    let scale = 0.2 + Math.min(scaleX, scaleY)
+    return scale;
 }
